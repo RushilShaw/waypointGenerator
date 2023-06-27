39cefabb83a88e07660e331cc4418cab9b32a30d
Rushil Shah <shah.rushil.s@gmail.com>
2023-06-27 16:38:53 -0400

    tested and fixed stream_file

--- stream_file.py
+++ stream_file.py
@@ -1,21 +1,22 @@
 import serial
+import serial.tools.list_ports
 import time
 
 
-FILE_NAME = "output.txt"
+FILE_NAME = "./output.txt"
 GPS_DEVICE_NAME = "Silicon Labs CP210x USB to UART Bridge"
 BAUD = 115200
 STREAM_DELAY = 0.5
 
 
-def steam_file(file_name, port, baud, stream_delay=0.5):
+def stream_file(file_name, port, baud, stream_delay=0.5):
     with (
         open(file_name, 'r') as file,
         serial.Serial(port, baudrate=baud) as ser
     ):
         time.sleep(1)
         for line in file.readline():
-            ser.write(line)
+            ser.write(line.encode())
             time.sleep(stream_delay)
 
 
@@ -27,4 +28,8 @@ def get_serial_port_by_device_name(device_name):
 
 def main():
     gps_device_port = get_serial_port_by_device_name(GPS_DEVICE_NAME)
-    steam_file(FILE_NAME, gps_device_port, BAUD, STREAM_DELAY)
+    stream_file(FILE_NAME, gps_device_port, BAUD, STREAM_DELAY)
+
+
+if __name__ == '__main__':
+    main()
